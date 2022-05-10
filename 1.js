df37ad1f6c9c0e53ea3c8d0a7e98d0d554054bca
Tsclever <yxt15850005248@outlook.com>
2022-05-10 21:37:19 +0800

    第一次尝试，失败，还没有掌握onwheel的用法

---
+++ 1.js
@@ -0,0 +1,7 @@
+let pa1 =  document.getElementById("page1");
+let pa2 =  document.getElementById("page2");
+
+onwheel = function roll() {
+    pa1.style.display = "none";
+    pa2.style.display = "block";
+}
